053c90fb347a25e38f291253166c1bc5118956f2
Abhishek V <scantyvabhishek@gmail.com>
2026-03-16 00:39:48 +0530

    Frontend & Backend connected using api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const noteModel = require('./models/note.model')
+const cors = require("cors")
 const app = express();
+app.use(cors())
 app.use(express.json());
+
 
 // APIs 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
+import axios from "axios"
 function App() {
+
+
+  const [notes, setNotes] = useState([
+    {
+      title: "Dummy Title-1",
+      description: "Sample Description for title-1"
+    },
+    {
+      title: "Test title-2",
+      description: "SAmple content"
+    },
+    {
+      title: "Test title-3",
+      description: "this is also some content "
+    },
+    {
+      title: "Test title-4",
+      description: "SAmple content"
+    }
+  ])
+
+  axios.get("http://localhost:3000/api/notes").then((res)=>{
+    // setNotes = res.notes
+    console.log(res.data.notes)
+
+    setNotes(res.data.notes)
+  })
 
   return (
     <>
-    Hello World !!!
+      <div className='notes'>
+        {
+          notes.map(note => {
+            return <div className="note">
+              <h2>{note.title}</h2>
+              <p>{note.description}</p>
+            </div>
+          })
+        }
+      </div>
+      
     </>
   )
 }
